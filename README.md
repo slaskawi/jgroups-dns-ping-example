93295b692a997e463e164f2a07d23edc7c497888
Sebastian Laskawiec <slaskawi@redhat.com>
2016-12-20 14:38:03 +0100

    DNS PING Work in progress

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
-# jgroups-dns-ping-example
+# How to run the example
+
+* Start OpenShift local installation `oc cluster up`.
+* Rebuild this project `mvn clean install`.
+* Publish artifacts to OpenShift `mvn fabric8:run`.
+* Install a service `oc create -f service.yaml`.
+* Verify logs `oc logs po/...`. There should be no exception in logs. The first Pod should become a coordinator.
+* Scale the pods `oc scale dc jgroups-dns-ping --replicas=5`.
+* Verify logs `oc logs po/...`. A new view should be installed.
+
+# Debugging
+
+* Use RHEL image with diagnosis tools: `oc run rheltest --image=registry.access.redhat.com/rhel7/rhel-tools --restart=Never --attach -i --tty`.
+* Use `dig +search jgroups-dns-ping.myproject.svc.cluster.local` and make sure that proper IPs are returned.
---
+++ src/main/java/org/infinispan/dns/JGroupsKubernetes.java
@@ -0,0 +1,38 @@
+package org.infinispan.dns;
+
+import java.io.InputStream;
+import java.util.concurrent.TimeUnit;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import org.jgroups.JChannel;
+import org.jgroups.ReceiverAdapter;
+import org.jgroups.View;
+
+public class JGroupsKubernetes {
+
+   private static final String CLUSTER_NAME = "CLUSTER_NAME";
+
+   public static void main(String[] args) throws Exception {
+
+      Logger rootLog = Logger.getLogger("");
+      rootLog.setLevel( Level.FINE );
+      rootLog.getHandlers()[0].setLevel( Level.FINE );
+
+      InputStream configuration = JGroupsKubernetes.class.getResourceAsStream("/config-test.xml");
+      JChannel channel = new JChannel(configuration);
+
+      channel.setReceiver(new ReceiverAdapter(){
+         @Override
+         public void viewAccepted(View view) {
+            System.out.println("#### VIEW: " + view);
+         }
+      });
+
+      channel.connect(CLUSTER_NAME);
+
+      TimeUnit.MINUTES.sleep(10);
+
+      channel.close();
+   }
+}
